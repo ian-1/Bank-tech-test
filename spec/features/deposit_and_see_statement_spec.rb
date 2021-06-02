7440bce9e30674f1d63adf7e433d5c84566aab01
ian-1 <iandavidwalton@mail.com>
2021-06-02 15:20:54 +0100

    Feature test 'make multiple deposits and see them within a statement' added, not yet passed

--- spec/features/deposit_and_see_statement_spec.rb
+++ spec/features/deposit_and_see_statement_spec.rb
@@ -2,10 +2,23 @@ require 'account'
 
 # rubocop:disable RSpec/DescribeClass
 describe 'Feature: Deposit and see statement' do
+  let(:ac) { Account.new }
+
   it 'make a deposit and see it within a statement' do
-    ac = Account.new
     ac.deposit(1.50, '01/01/2000')
     output = "date || credit || debit || balance\n01/01/2000 || 1.50 || || 1.50"
+    expect(ac.statement).to eq(output)
+  end
+
+  it 'make multiple deposits and see them within a statement' do
+    ac.deposit(9, '10/06/2020')
+    ac.deposit(9.99, '11/06/2020')
+    ac.deposit(99.1, '11/06/2020')
+    header = 'date || credit || debit || balance'
+    line_one = "\n11/06/2020 || 99.10 || || 118.09"
+    line_two = "\n11/06/2020 || 9.99 || || 18.99"
+    line_three = "\n10/06/2020 || 9.00 || || 9.00"
+    output = header + line_one + line_two + line_three
     expect(ac.statement).to eq(output)
   end
 end
